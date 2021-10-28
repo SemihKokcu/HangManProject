a2b7b675dee123e2664bf02ece8e40e36ac992fd
SemihKÖKÇÜ <semih.kokcu@mf.karaelmas.edu.tr>
2021-10-28 20:14:03 +0300

    TitleUptade

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HangMan from '../hang-man/HangMan';
 import "./App.css"
 
 
-function App() {
-  return (
-   <div className="App">
-     
-     <HangMan></HangMan>
-     
-   </div>
-  );
+
+class App extends Component {
+       
+  componentDidMount(){
+    document.title = "Hangman"
+  }
+  render() {
+    return (
+      <div className="App">
+        <HangMan></HangMan> </div>
+    );
+  }
 }
 
 export default App;
